4ba5c1c479673b8ca4d3f3f2273629eed361926a
paulo.gomes <dbc.pgomes@unimedpoa.com.br>
2022-12-03 01:21:19 -0300

    Criação do projeto.

---
+++ src/main/java/com/br/listas/ListasApplication.java
@@ -0,0 +1,13 @@
+package com.br.listas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListasApplication.class, args);
+	}
+
+}
